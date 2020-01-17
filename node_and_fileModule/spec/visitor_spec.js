da770327a76c91000acde43636f9f2ac8e6c6547
Sbonelo <37469171+Sbonelo01@users.noreply.github.com>
2020-01-17 10:30:31 +0200

    Update visitor_spec.js
    refactored specs

--- node_and_fileModule/spec/visitor_spec.js
+++ node_and_fileModule/spec/visitor_spec.js
@@ -6,7 +6,7 @@ describe('functionality tests', () => {
 		let charlie = new visitor.Visitor();
 
 		let alice = new visitor.Visitor('Alice Smith', 21, '13/01/2020', '12:00', 'amazing', 'Sbonelo');
-		let data = '{"fullName":"Alice Smith","age":21,"date":"13-01-2020","time":"12:00","comments":"amazing","person":"Sbonelo"}';
+        	let data = '{"fullName":"Alice Smith","age":21,"date":"13/01/2020","time":"12:00","comments":"amazing","person":"Sbonelo"}';
 
 	it('should exist', () => {
 		expect(visitor.load).toBeDefined();
@@ -16,9 +16,11 @@ describe('functionality tests', () => {
 		expect(bob.save).toBeDefined();
 		expect(charlie.save).toBeDefined();
 	});
-
-	xit('should return data', () => {
-		expect(JSON.stringify(alice)).toBe(data);	
+	
+	it('should return data', () => {
+        	expect(JSON.parse(JSON.stringify(alice))).toEqual(JSON.parse(data));    
+    });
+	
 	});
 	
 	it('writes', () => {
