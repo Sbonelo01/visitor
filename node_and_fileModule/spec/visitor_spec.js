cef86c02cd7a667ccd5bc537027af33db12b09dc
Sbonelo <37469171+Sbonelo01@users.noreply.github.com>
2020-01-21 14:32:04 +0200

    Update visitor_spec.js
    Added a test to check if the data matches

--- node_and_fileModule/spec/visitor_spec.js
+++ node_and_fileModule/spec/visitor_spec.js
@@ -2,7 +2,15 @@ let visitor = require('../src/visitor');
 let fs = require('fs');
 
 describe('function save()', () => {
-    let alice = new visitor.Visitor('alice_cooper', 21, '13/01/2020', '12:00', 'amazing', 'Sbonelo')
+    let alice = new visitor.Visitor('alice_cooper', 21, '13/01/2020', '12:00', 'amazing', 'Sbonelo');
+    let visitorData = {
+      'fullName': 'alice',
+      'age': 24,
+      'date': '21/12/2020',
+      'time': '12:12',
+      'comments': 'amazing',
+      'person': 'Sbonelo'
+    }
     
     it('exists', () => {
         expect(alice.save).toBeDefined()
@@ -16,6 +24,14 @@ describe('function save()', () => {
                 expect(readData).toEqual(JSON.stringify(alice, null, 4))
             }
         })
+    })
+    
+    it('should match the data', () => {
+        expect(visitorData.age).toEqual(24);
+        expect(visitorData.date).toEqual('21/12/2020');
+        expect(visitorData.time).toEqual('12:12');
+        expect(visitorData.comments).toEqual('amazing');
+        expect(visitorData.person).toEqual('Sbonelo')
     })
 
 })
